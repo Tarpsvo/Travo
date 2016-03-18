549b5e87a20724f2257fff5ef3fa7e5e462f451d
Tarvo R <tarvoreinpalu@gmail.com>
2016-03-19 00:31:28 +0200

    Fixed task naming in watch.

--- Travo.FrontEnd/build/tasks/watch.js
+++ Travo.FrontEnd/build/tasks/watch.js
@@ -15,8 +15,8 @@ gulp.task('copy-lib-w-reload', ['copy-lib'], browserSync.reload);
 // Watches changes to JS, HTML and CSS files and call the reportChange method
 gulp.task('watch', ['serve'], function() {
     gulp.watch(paths.ts, ['build-system-w-reload']).on('change', reportChange);
-    gulp.watch(paths.html, ['build-html-w-reload']).on('change', reportChange);
+    gulp.watch(paths.html, ['copy-html-w-reload']).on('change', reportChange);
+    gulp.watch(paths.lib, ['copy-lib-w-reload']).on('change', reportChange);
     gulp.watch(paths.scss, ['build-scss']).on('change', reportChange);
     gulp.watch(paths.img, ['copy-images']).on('change', reportChange);
-    gulp.watch(paths.lib, ['copy-lib']).on('change', reportChange);
 });
