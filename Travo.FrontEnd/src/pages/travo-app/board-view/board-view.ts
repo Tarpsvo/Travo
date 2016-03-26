031f2b9831de2edd78926b43779e03589af28ba9
Tarvo R <tarvoreinpalu@gmail.com>
2016-03-26 23:32:48 +0200

    Added task addition design and basic functionality to front-end.

--- Travo.FrontEnd/src/pages/travo-app/board-view/board-view.ts
+++ Travo.FrontEnd/src/pages/travo-app/board-view/board-view.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../lib/autosize/jquery.autosize.d.ts" />
+
 import {inject} from 'aurelia-framework';
 import Notify from 'notify-client';
 import BoardServices from 'services/board-services';
@@ -8,6 +10,7 @@ export class BoardView {
     boardServices: BoardServices;
 
     // View values
+    autosizeOn = false;
     tagsWithTasks: Object[];
 
     constructor(boardServices: BoardServices) {
@@ -16,6 +19,18 @@ export class BoardView {
 
     activate(params) {
         this.getTagsWithTasks(params.id);
+    }
+
+    userOpenedTaskForm(index: number) {
+        var textArea = this.getTextAreaFromIndex(index);
+
+        if (!this.autosizeOn) {
+            this.autosizeOn = true;
+            autosize(textArea);
+        }
+
+        textArea.focus();
+        textArea.value = "";
     }
 
     getTagsWithTasks(boardId: number) {
@@ -28,5 +43,36 @@ export class BoardView {
             .catch(response => {
                 console.log(response);
             });
+    }
+
+    addNewTask(index: number) {
+        var textArea = this.getTextAreaFromIndex(index);
+        var taskText = textArea.value;
+
+        if (!taskText || taskText.trim().length == 0) {
+            return;
+        }
+
+        var newTask = {
+            Description: taskText
+        };
+
+        var taskArray = (<Object[]> this.tagsWithTasks[index]['Tasks']);
+        taskArray.push(newTask);
+        this.closeTextAreaAtIndex(index);
+    }
+
+    closeTextAreaAtIndex(index: number) {
+        let wrapperId = 'trv-ct-form-' + index;
+        let closeAnchor = document.getElementById(wrapperId).getElementsByClassName('trv-cancel-btn')[0];
+        var evt = document.createEvent("HTMLEvents");
+        evt.initEvent("click", true, true);
+        closeAnchor.dispatchEvent(evt);
+    }
+
+    getTextAreaFromIndex(index: number) {
+        let wrapperId = 'trv-ct-form-' + index;
+        let textArea = (<HTMLInputElement> document.getElementById(wrapperId).querySelector('textarea'));
+        return textArea;
     }
 }
